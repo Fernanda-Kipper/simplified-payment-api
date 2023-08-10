b063800d88fb3c757a18251335902af34a5a358d
Fernanda Kipper <nanda.kipper@gmail.com>
2023-08-10 11:37:01 -0300

    feat: create TransactionRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -0,0 +1,7 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
