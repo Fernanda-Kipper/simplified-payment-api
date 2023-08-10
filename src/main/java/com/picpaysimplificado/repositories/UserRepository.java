c4ce1674b6b61f4e9809e562984ee7e0fd60ce80
Fernanda Kipper <nanda.kipper@gmail.com>
2023-08-10 11:36:06 -0300

    feat: create UserRepository

---
+++ src/main/java/com/picpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
