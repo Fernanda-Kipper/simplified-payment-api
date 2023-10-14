ea8e7ac223c7fbc58b65201eaab89627b020d53f
Fernanda Kipper <nanda.kipper@gmail.com>
2023-10-14 19:31:02 -0300

    feat: add unit tests to UserRepository

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,45 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if(sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuário do tipo Lojista não está autorizado a realizar transação");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.repository.findAll();
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
---
+++ src/test/java/com/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get User successfully from DB")
+    void findUserByDocumentCase1() {
+        String document = "99999999901";
+        UserDTO data = new UserDTO("Fernanda", "Teste", document, new BigDecimal(10), "test@gmail.com", "44444", UserType.COMMON);
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get User from DB when user not exists")
+    void findUserByDocumentCase2() {
+        String document = "99999999901";
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
+    }
+}
