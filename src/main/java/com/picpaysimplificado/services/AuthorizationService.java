921fed406da5f535a4dfc87059b46ed3dc1c96fa
Fernanda Kipper <nanda.kipper@gmail.com>
2023-10-15 13:25:44 -0300

    refactor: extract AuthorizationService API URL to application.properties

--- src/main/java/com/picpaysimplificado/services/AuthorizationService.java
+++ src/main/java/com/picpaysimplificado/services/AuthorizationService.java
@@ -2,6 +2,7 @@ package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,8 +16,11 @@ public class AuthorizationService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${app.authorizationApi}")
+    private String authApiUrl;
+
     public boolean authorizeTransaction(User sender, BigDecimal value){
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
+        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity(this.authApiUrl, Map.class);
 
         if(authorizationResponse.getStatusCode() == HttpStatus.OK){
             String message = (String) authorizationResponse.getBody().get("message");
